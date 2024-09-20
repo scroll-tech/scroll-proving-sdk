7440c7c1a09ec7b0d2ef87357bc2359e3f4d36b3
HAOYUatHZ <haoyu@protonmail.com>
2024-09-20 15:14:16 +1000

    more key_signer

--- src/coordinator_handler/key_signer.rs
+++ src/coordinator_handler/key_signer.rs
@@ -1,11 +1,18 @@
 use ethers_core::{
-    k256::{ecdsa::SigningKey, PublicKey, SecretKey},
-    utils::hex,
+    k256::{
+        ecdsa::{signature::hazmat::PrehashSigner, RecoveryId, Signature, SigningKey},
+        elliptic_curve::{sec1::ToEncodedPoint, FieldBytes},
+        PublicKey, Secp256k1, SecretKey,
+    },
+    types::Signature as EthSignature,
+    types::{H256, U256},
+    utils::{hex, hex::ToHex},
 };
 use rand;
 use rand::RngCore;
 use std::fs::File;
 use std::io::{Read, Write};
+use tiny_keccak::{Hasher, Keccak};
 
 const DEFAULT_KEY_SIZE: usize = 32usize;
 
@@ -50,7 +57,59 @@ impl KeySigner {
         })
     }
 
-    pub fn sign(&self, data: &[u8]) -> anyhow::Result<String> {
-        todo!()
+    pub fn get_public_key(&self) -> String {
+        let v: Vec<u8> = Vec::from(self.public_key.to_encoded_point(true).as_bytes());
+        buffer_to_hex(&v, false)
     }
+
+    /// Signs the provided hash.
+    pub fn sign_hash(&self, hash: H256) -> anyhow::Result<EthSignature> {
+        let signing_key = &self.signing_key as &dyn PrehashSigner<(Signature, RecoveryId)>;
+        let (recoverable_sig, recovery_id) = signing_key.sign_prehash(hash.as_ref())?;
+
+        let v = u8::from(recovery_id) as u64;
+
+        let r_bytes: FieldBytes<Secp256k1> = recoverable_sig.r().into();
+        let s_bytes: FieldBytes<Secp256k1> = recoverable_sig.s().into();
+        let r = U256::from_big_endian(r_bytes.as_slice());
+        let s = U256::from_big_endian(s_bytes.as_slice());
+
+        Ok(EthSignature { r, s, v })
+    }
+
+    pub fn sign_buffer<T>(&self, buffer: &T) -> anyhow::Result<String>
+    where
+        T: AsRef<[u8]>,
+    {
+        let pre_hash = keccak256(buffer);
+
+        let hash = H256::from(pre_hash);
+        let sig = self.sign_hash(hash)?;
+
+        Ok(buffer_to_hex(&sig.to_vec(), true))
+    }
+}
+
+fn buffer_to_hex<T>(buffer: &T, has_prefix: bool) -> String
+where
+    T: AsRef<[u8]>,
+{
+    if has_prefix {
+        format!("0x{}", buffer.encode_hex::<String>())
+    } else {
+        buffer.encode_hex::<String>()
+    }
+}
+
+/// Compute the Keccak-256 hash of input bytes.
+///
+/// Note that strings are interpreted as UTF-8 bytes,
+pub fn keccak256<T: AsRef<[u8]>>(bytes: T) -> [u8; 32] {
+    let mut output = [0u8; 32];
+
+    let mut hasher = Keccak::v256();
+    hasher.update(bytes.as_ref());
+    hasher.finalize(&mut output);
+
+    output
 }
