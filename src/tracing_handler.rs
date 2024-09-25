36f24c57faab926d61e9d143acbcbb96783c3de1
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2024-09-25 15:33:20 +1000

    feat(tracing handler): implement `get_sorted_traces_by_hashes` (#8)
    Co-authored-by: Zhuo Zhang <mycinbrin@gmail.com>

--- src/tracing_handler.rs
+++ src/tracing_handler.rs
@@ -4,6 +4,7 @@ use ethers_core::types::H256;
 use ethers_providers::{Http, Provider};
 use prover_darwin_v2::BlockTrace;
 use serde::{de::DeserializeOwned, Serialize};
+use std::cmp::Ordering;
 use std::fmt::Debug;
 use tokio::runtime::Runtime;
 
@@ -59,8 +60,56 @@ impl L2gethClient {
 
     pub fn get_sorted_traces_by_hashes(
         &self,
-        hashes: &[CommonHash],
+        block_hashes: &[CommonHash],
     ) -> anyhow::Result<Vec<BlockTrace>> {
-        todo!()
+        if block_hashes.is_empty() {
+            log::error!("failed to get sorted traces: block_hashes are empty");
+            anyhow::bail!("block_hashes are empty")
+        }
+
+        let mut block_traces = Vec::new();
+        for hash in block_hashes.iter() {
+            let trace = self.get_block_trace_by_hash_sync(hash)?;
+            block_traces.push(trace);
+        }
+
+        block_traces.sort_by(|a, b| {
+            if get_block_number_from_trace(a).is_none() {
+                Ordering::Less
+            } else if get_block_number_from_trace(b).is_none() {
+                Ordering::Greater
+            } else {
+                get_block_number_from_trace(a)
+                    .unwrap()
+                    .cmp(&get_block_number_from_trace(b).unwrap())
+            }
+        });
+
+        let block_numbers: Vec<u64> = block_traces
+            .iter()
+            .map(|trace| get_block_number_from_trace(trace).unwrap_or(0))
+            .collect();
+        let mut i = 0;
+        while i < block_numbers.len() - 1 {
+            if block_numbers[i] + 1 != block_numbers[i + 1] {
+                log::error!(
+                    "block numbers are not continuous, got {} and {}",
+                    block_numbers[i],
+                    block_numbers[i + 1]
+                );
+                anyhow::bail!(
+                    "block numbers are not continuous, got {} and {}",
+                    block_numbers[i],
+                    block_numbers[i + 1]
+                )
+            }
+            i += 1;
+        }
+
+        Ok(block_traces)
     }
+}
+
+fn get_block_number_from_trace(block_trace: &BlockTrace) -> Option<u64> {
+    block_trace.header.number.map(|n| n.as_u64())
 }
