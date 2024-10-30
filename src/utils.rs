dfbb6ec0153297d4a313bec7412a9a329b34cd7b
HAOYUatHZ <haoyu@protonmail.com>
2024-10-30 12:06:10 +1100

    minor

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 const SDK_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub fn get_version(circuit_version: &str) -> String {
-    format!("sdk-{}-{}", SDK_VERSION, circuit_version)
+    format!("sdk-v{}-{}", SDK_VERSION, circuit_version)
 }
 
 pub fn init_tracing() {
