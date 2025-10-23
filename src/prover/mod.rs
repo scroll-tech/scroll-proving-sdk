05648db3a6bcc19bfe58ff493176714d6a0553db
Noel <noelwei@gmail.com>
2025-10-23 11:11:09 +0900

    fix more

--- src/prover/mod.rs
+++ src/prover/mod.rs
@@ -97,7 +97,7 @@ where
                     .handle_task(coordinator_client, Some((task_type, task_str.as_str())))
                     .await
                 {
-                    error!(?prover_name, ?e, "Error handling task");
+                    error!(prover_name, error = e.to_string(), "Error handling task");
                     panic!("task fail");
                 }
                 i
@@ -126,7 +126,7 @@ where
             info!(?prover_name, "Getting task from coordinator");
 
             if let Err(e) = self.handle_task(coordinator_client, None).await {
-                error!(prover_name, e, "Error handling task");
+                error!(prover_name, error = e.to_string(), "Error handling task");
             }
 
             sleep(Duration::from_secs(WORKER_SLEEP_SEC)).await;
